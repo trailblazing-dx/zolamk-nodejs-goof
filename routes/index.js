09ab71571fd88b442ae7138f6248fdd2023a4f94
Zelalem Mekonen <zola@programmer.net>
2023-12-07 09:38:05 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -19,23 +19,6 @@ var fs = require('fs');
 // prototype-pollution
 var _ = require('lodash');
 
-exports.loginHandler = function (req, res, next) {
-  if (validator.isEmail(req.body.username)) {
-    User.find({ username: req.body.username, password: req.body.password }, function (err, users) {
-      if (users.length > 0) {
-        const redirectPage = req.body.redirectPage
-        const session = req.session
-        const username = req.body.username
-        return adminLoginSuccess(redirectPage, session, username, res)
-      } else {
-        return res.status(401).send()
-      }
-    });
-  } else {
-    return res.status(401).send()
-  }
-};
-
 function adminLoginSuccess(redirectPage, session, username, res) {
   session.loggedIn = 1
 
