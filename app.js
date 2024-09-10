f6f772f09d4a6578c0d17df7129dfde6f570afa4
Zelalem Mekonen <zola@programmer.net>
2024-09-10 14:24:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -81,6 +81,8 @@ if (app.get('env') == 'development') {
 }
 
 var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
+
+var password_token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
 console.log('token: ' + token);
 
 http.createServer(app).listen(app.get('port'), function () {
